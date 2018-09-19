6fb3225debe88fa652141ee56f58e525ce2dbac1
Adit Patel <MyFolder@Adits-MacBook-Air.local>
2018-09-18 20:44:59 -0400

    updating routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,6 +3,7 @@ const articleRoutes = require("./articles");
 const noteRoutes = require("./notes");
 
 // Article routes
+router.use("/article", articleRoutes);
 router.use("/note", noteRoutes);
 
 module.exports = router;
